f62fb4eff8c276ad290776c56bc197f346136978
wjonasreger <wjonasreger@gmail.com>
2023-04-19 14:46:55 -0500

    removed example datasets and associated code

--- R/composeData.R
+++ R/composeData.R
@@ -21,19 +21,6 @@
 #' composeData(verbose = FALSE, merge_verbose = FALSE)
 #' }
 composeData = function(verbose = FALSE, merge_verbose = FALSE) {
-  # compose and convert ex_data
-  if (verbose) cat(paste("Saving objects:", "  ex_data.Rdata", "", sep = "\n"))
-  ex_data_path = system.file("extdata/ex_data", package = "mousePreopticR")
-  ex_data_targ = system.file("extdata", package = "mousePreopticR")
-  ex_data = csvMerge(data_dir = ex_data_path, axis = 0, verbose = merge_verbose)
-  save(ex_data, file = file.path(ex_data_targ, "ex_data.Rdata"))
-
-  # compose and convert ex_matrix
-  if (verbose) cat(paste("Saving objects:", "  ex_matrix.Rdata", "", sep = "\n"))
-  ex_matrix_path = system.file("extdata/ex_matrix", package = "mousePreopticR")
-  ex_matrix_targ = system.file("extdata", package = "mousePreopticR")
-  ex_matrix = matrixMerge(data_dir = ex_matrix_path, verbose = merge_verbose)
-  save(ex_matrix, file = file.path(ex_matrix_targ, "ex_matrix.Rdata"))
 
   # convert GEO_barcodes.tsv
   if (verbose) cat(paste("Saving objects:", "  GEO_barcodes.Rdata", "", sep = "\n"))

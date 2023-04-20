3f8d42e249e6b19e989df7cba5492bad43fd5141
wjonasreger <wjonasreger@gmail.com>
2023-04-20 01:40:56 -0500

    removed code for loading example data. modified some values for parameters

--- R/loadData.R
+++ R/loadData.R
@@ -10,26 +10,19 @@
 #'    - GEO_matrix
 #'    - DRYAD_data
 #'
-#' @param datasets Character vector to select which datasets to load - one of 'all', 'bio', or 'example' (Default: "bio")
+#' @param datasets Character vector to select which datasets to load - one of 'all', 'bio', 'GEO' or 'DRYAD' (Default: "all")
 #' @param verbose A boolean to print result summary for loading objects (Default: FALSE)
 #'
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#' loadData(datasets = "bio", verbose = FALSE)
+#' loadData(datasets = "all", verbose = FALSE)
 #' }
 loadData = function(datasets = "all", verbose = FALSE) {
-  stopifnot("`datasets` must be one of 'all', 'bio', or 'example'" = any(datasets == c("all", "bio", "example")))
-  if (datasets == "all" | datasets == "example") {
-    # load ex_data
-    load(system.file("extdata/ex_data.Rdata", package = "mousePreopticR"), envir = globalenv(), verbose = verbose)
+  stopifnot("`datasets` must be one of 'all', 'bio', 'GEO' or 'DRYAD'" = any(datasets == c("all", "bio", "GEO", "DRYAD")))
 
-    # load ex_matrix
-    load(system.file("extdata/ex_matrix.Rdata", package = "mousePreopticR"), envir = globalenv(), verbose = verbose)
-  }
-
-  if (datasets == "all" | datasets == "bio") {
+  if (datasets %in% c("all", "bio", "GEO")) {
     # load GEO_barcodes
     load(system.file("extdata/GEO_barcodes.Rdata", package = "mousePreopticR"), envir = globalenv(), verbose = verbose)
 
@@ -38,7 +31,9 @@ loadData = function(datasets = "all", verbose = FALSE) {
 
     # load GEO_matrix
     load(system.file("extdata/GEO_matrix.Rdata", package = "mousePreopticR"), envir = globalenv(), verbose = verbose)
+  }
 
+  if (datasets %in% c("all", "bio", "DRYAD")) {
     # load DRYAD_data
     load(system.file("extdata/DRYAD_data.Rdata", package = "mousePreopticR"), envir = globalenv(), verbose = verbose)
   }
